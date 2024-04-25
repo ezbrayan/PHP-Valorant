d8b44aeea9f5603f456fda6e1f507b2e4a26477a
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-04-24 20:21:13 -0500

    ajuste de estadisticas

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Assets/css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>DESKTOP-WINDOWS</title>
+    <title>DESKTOP-WINDOWS2</title>
 </head>
 
 <body>
